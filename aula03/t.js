1cff9b3bff62316e06de8e0611ffbbcbf478f094
Guusta2024 <jose.gustavo1@escolar.ifrn.edu.br>
2024-07-05 08:29:07 -0300

    retorno

---
+++ aula03/t.js
@@ -0,0 +1,9 @@
+
+
+function sub(){
+    let nome = document.getElementById("nome").value;
+    let sobrenome = document.getElementById("sobrenome").value;
+    document.getElementById("resultado").innerHTML = 
+        "<h3>seu nome completo: " + nome + " " + sobrenome 
+    
+    }
