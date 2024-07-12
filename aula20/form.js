03f218d01abf360d069357e1c4f5da34606a051c
Guusta2024 <jose.gustavo1@escolar.ifrn.edu.br>
2024-07-12 08:21:26 -0300

    modificaçoes

--- aula20/form.js
+++ aula20/form.js
@@ -1,9 +1,16 @@
 function mandar(field) {
+    let senha = document.getElementById("senha").value;
+    let csenha = document.getElementById("csenha").value;
     var nome = document.getElementById("nome").value;
     var email = document.getElementById("email").value;
+    if (csenha == senha){
     document.getElementById("resultado").innerHTML = 
-        "<h3> Informações Recebidas</h3>" +
-        "<p>Nome: " + nome + "</p>" +
-        "<p>E-mail: "+ email + "</p>";
+        `<h3> Informações Recebidas</h3>
+        <p>Nome: ${nome}</p>
+        <p>E-mail: ${email}</p>
+        <p>senha: ${senha} é valida </p>`
+    }else{
+        alert("senha invalida")
+    }
 
 }
